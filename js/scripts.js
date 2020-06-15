71918850bddd938c78361b35672e79550721e3d9
kavya <kavya6841@gmail.com>
2020-06-15 06:48:17 -0400

    npm scripts1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,36 @@
-<script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();
+  $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip()
         });
-    </script>
-        /*<script>
+    
+    $("#reserve").click(function(){
+
+    $('#reserveform').modal('show')
+     });  
+
+            $("#login").click(function(){
+           
+               $('#loginModal').modal('show')
+                });  
+        
+        $("#carouselButton").click(function(){
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) 
+            {
+
+                   $("#mycarousel").carousel('pause');
+                   
+                   $("#carouselButton").children("span").removeClass('fa-pause');
+                   $("#carouselButton").children("span").addClass('fa-play');
+            }
+            else if ($("#carouselButton").children("span").hasClass('fa-play'))
+            {
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');                    
+            }
+        });
+  
+
+      /*<script>
             $(document).ready(function(){
                 $("#mycarousel").carousel( { interval: 2000 } );
                 $("#carousel-pause").click(function(){
@@ -14,37 +41,3 @@
                 });
             });
         </script>*/
-<script>
-
- $("#reserve").click(function(){
-
-    $('#reserveform').modal('show')
-     });  
-        </script>
-        <script>
-
-            $("#login").click(function(){
-           
-               $('#loginModal').modal('show')
-                });  
-                   </script>
-        
-
-     <script>
-        $(document).ready(function(){
-            $("#mycarousel").carousel( { interval: 500 } );
-            $("#carouselButton").click(function(){
-                if( $("#carouselButton").children('span').hasClass('fa-pause')){
-                    $("#mycarousel").carousel('pause');
-                    $("#carouselButton").children('span').removeClass('fa-pause')
-                    $("#carouselButton").children('span').addClass('fa-play')
-                }
-                else if( $("#carouselButton").children('span').hasClass('fa-play')){
-                    $("#mycarousel").carousel('cycle');
-                    $("#carouselButton").children('span').removeClass('fa-play')
-                    $("#carouselButton").children('span').addClass('fa-pause')
-                }
-                
-            });
-        });
-    </script>
